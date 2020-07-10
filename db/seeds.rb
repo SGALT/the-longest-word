faddb880980d04eda1707a63d1f910de1fe13759
Sylvain Galtier <sylvaingaltier@gmail.com>
2020-07-10 10:18:06 +0200

    models controllers and routes are ok for now, missing parties validation for 5 max inside each game

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# puts "Testing parties limitation"
+# Game.destroy_all
+
+# puts "Creating games"
+# Game.create(user_id: 1)
+# puts "Game created"
+# # byebug
+# puts "trying to creating five parties"
+# 4.times do
+#   Party.create(game_id: Game.first.id, ten_letters_list: ('a'..'z').to_a.sample(8).join(''), word: ('a'..'z').to_a.sample(8).join('') )
+# end
+# puts "5 parties were created"
+
+# puts "now we are trying to create one more party, hoping taht it will fail"
+# Party.create(game_id: Game.first.id, ten_letters_list: ('a'..'z').to_a.sample(8).join(''), word: ('a'..'z').to_a.sample(8).join('') )
+# puts "If you see this message, too bad for you, go back to work your rails basics"
---
+++ test/controllers/game_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class GameControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get game_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get game_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get game_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get game_create_url
+    assert_response :success
+  end
+
+end
