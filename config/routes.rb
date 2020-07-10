31bbfa29c336ae78273f7394208684e20d4615c3
Sylvain Galtier <sylvaingaltier@gmail.com>
2020-07-10 15:36:25 +0200

    reducing routes for games and parties, removal of index for each, set of games view with game list with game evolution and a button to create new game inside games index view, initiated games show view with first data inside and notice for later branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'games#index'
 
-  resources :games, only: [:show, :new, :create] do
-    resources :parties, only: [:show, :new, :create] do
+  resources :games, only: [:show, :create] do
+    resources :parties, only: [:show, :create] do
       resources :solutions, only: [:index]
     end
   end
