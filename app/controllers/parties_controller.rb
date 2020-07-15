5537a216516a9c966dd7405f851eb39407550f46
Sylvain Galtier <sylvaingaltier@gmail.com>
2020-07-15 15:21:55 +0200

    in game controller, change english to french, in parties controller set of new and create instead of edit and update to stick more to the rules, set of ten letters list at initialize and set hidden field in new view with the ten letters list value, creation of party show view and route to see your answer, the score and to see the best solutions

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,25 +1,42 @@
 class PartiesController < ApplicationController
-  def edit
+  def show
     @party = Party.find(params[:id])
   end
 
-  def update
-    @party = Party.find(params[:id])
-    @game = @party.game
+  def new
+    @party = Party.new
+    @ten_letters_list = ten_letters_list!
+  end
+
+  def create
+    @party = Party.new(party_params)
+    @party.game_id = params[:game_id]
+    @party.available = true
     @try = params[:party][:word]
     if word_valid?(@try, @party.ten_letters_list) && word_exists?(@try)
       @party.party_score = @try.size
-      @party.update(party_params)
-      redirect_to game_path(@game), notice: "Great, you just gained #{@try.size} points"
+      @party.save
+      redirect_to game_path(@party.game), notice: "Bravo, vous venez de marquer #{@try.size} points"
     else
-      redirect_to edit_game_party_path(@game, @party), notice: 'Not Great'
+      redirect_to new_game_party_path(@party.game), notice: "mot non existant ou vous n'utilisez pas les lettres proposées"
     end
   end
 
   private
 
   def party_params
-    params.require(:party).permit(:word)
+    params.require(:party).permit(:word, :ten_letters_list, :game_id)
+  end
+
+  def ten_letters_list!
+    @vowel = ['a', 'e', 'i', 'o', 'u', 'y']
+    @consonant = ('a'..'z').to_a - @vowel
+    result = []
+    5.times do
+      result << @vowel.sample
+      result << @consonant.sample
+    end
+    result.shuffle.join('')
   end
 
   def word_valid?(word, ten_letters_list)
